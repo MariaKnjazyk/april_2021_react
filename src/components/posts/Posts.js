f04bb8ed5d7d42d791c7ed715a5f8c8221036d6e
MariaKnjazyk <mashaknjazyk@gmail.com>
2021-06-30 11:26:40 +0300

    Homework 3

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -2,29 +2,48 @@ import {useEffect, useState} from "react";
 import {getPostComm, getItems} from "../../services/API";
 import Post from "../post/Post";
 import Comment from "../comment/Comment";
+import {useReducer} from "react";
+
+function reducer(state, action){
+    switch (action.type){
+        case 'POST_COMM': return {...state, postComm:action.payload};
+        case 'POSTS': return {...state, posts:action.payload};
+        default: return {...state};
+    }
+}
 
 export default function Posts() {
-    let [postComm, setPostComm]=useState();
+    let [state, dispatch]=useReducer(reducer, {postComm:[], posts:[]});
+
     let postFn=(id)=>{
-        getPostComm(id).then(value=>setPostComm(value));
+        getPostComm(id).then(value=>dispatch({type:'POST_COMM', payload: value}));
     };
 
-    let [posts, setPosts]=useState([]);
     useEffect(()=>{
-        getItems('posts').then(value => setPosts(value));
+        getItems('posts').then(value => dispatch({type:'POSTS',payload: value}));
     },[]);
+
+    // let [postComm, setPostComm]=useState();
+    // let postFn=(id)=>{
+    //     getPostComm(id).then(value=>setPostComm(value));
+    // };
+    //
+    // let [posts, setPosts]=useState([]);
+    // useEffect(()=>{
+    //     getItems('posts').then(value => setPosts(value));
+    // },[]);
 
 
 
     return (
         <div>
             {
-                posts.map((value) => <Post key={value.id} item={value} fnFather={postFn}/>)
+                state.posts.map((value) => <Post key={value.id} item={value} fnFather={postFn}/>)
             }
 
             <hr/>
             {
-                postComm && postComm.map(value=><Comment key={value.id} item={value}/>)
+                state.postComm && state.postComm.map(value=><Comment key={value.id} item={value}/>)
             }
 
         </div>
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,27 +2,46 @@ import User from '../user/User';
 import Post from '../post/Post'
 import {useEffect, useState} from "react";
 import {getUserPosts, getItems} from "../../services/API";
+import {useReducer} from "react";
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'USER_POSTS': return {...state, userPosts: action.payload};
+        case 'USERS': return {...state, users:action.payload};
+        default: return {...state};
+    }
+}
 
 export default function Users() {
+    let [state, dispatch]=useReducer(reducer, {userPosts:[], users:[]})
 
-    let [userPosts, setUserPosts]=useState();
     let userFn=(id)=>{
-        getUserPosts(id).then(value=>setUserPosts(value));
+        getUserPosts(id).then(value=>dispatch({type:'USER_POSTS', payload: value}));
     };
-    let [users, setUsers]=useState([]);
+
     useEffect(()=>{
-        getItems('users').then(value => setUsers(value));
+        getItems('users').then(value => dispatch({type:'USERS', payload: value}));
     },[]);
+
+
+    // let [userPosts, setUserPosts]=useState();
+    // let userFn=(id)=>{
+    //     getUserPosts(id).then(value=>setUserPosts(value));
+    // };
+    // let [users, setUsers]=useState([]);
+    // useEffect(()=>{
+    //     getItems('users').then(value => setUsers(value));
+    // },[]);
 
     return (
         <div>
             {
-                users.map((value) => <User key={value.id} item={value} fnFather={userFn}/>)
+                state.users.map((value) => <User key={value.id} item={value} fnFather={userFn}/>)
             }
 
             <hr/>
             {
-                userPosts && userPosts.map(value=><Post key={value.id} item={value}/>)
+                state.userPosts && state.userPosts.map(value=><Post key={value.id} item={value}/>)
             }
 
         </div>
